c1784bc62e2f87b48280b5c28ecb28205f4b4773
vignesh <vigneshvicky22476@gmail.com>
2024-10-01 16:19:23 +0530

    basis

---
+++ index.js
@@ -0,0 +1,19 @@
+ console.log("hi welcome")
+var a = 10
+var b = 20
+var result = a + b
+console.log("total value",result); 
+
+// primitve type 
+var num1 = 10
+var num2 = num1
+// re assign
+num1 = 20
+console .log("num1 :",num1);
+console .log("num2 :",num2);
+// reference type
+var renum1 = [10]
+var renum2 = renum1
+renum1[0] = 20
+console.log("renum1:",renum1);
+console.log("renum2:",renum2); 
